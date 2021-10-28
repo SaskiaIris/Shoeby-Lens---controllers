42e9868c11a9166a5475d210e27f34832bf31ec7
Scott Marchant <scott.marchant7@gmail.com>
2021-10-28 16:17:42 +0200

    made a button

---
+++ Assets/Scripts/ButtonMap.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OculusSampleFramework;
+using UnityEngine.Events;
+
+public class ButtonMap : MonoBehaviour
+{
+
+    public UnityEvent spawnMap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<ButtonController>().InteractableStateChanged.AddListener(InitiateEvent);
+    }
+
+    void InitiateEvent(InteractableStateArgs state)
+    {
+        if(state.NewInteractableState == InteractableState.ContactState)
+        {
+            spawnMap.Invoke();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
